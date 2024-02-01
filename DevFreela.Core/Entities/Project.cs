31dc02e8494e03fa21f24f5ea2c1467607850f41
monegasco <mhoa.2000@gmail.com>
2024-02-01 12:53:44 -0300

    avançando na camada core

---
+++ DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,27 @@
+﻿using DevFreela.Core.Enums;
+
+namespace DevFreela.Core.Entities;
+
+public class Project : BaseEntity
+{
+    public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost) : base()
+    {
+        Title = title;
+        Description = description;
+        IdClient = idClient;
+        IdFreelancer = idFreelancer;
+        TotalCost = totalCost;
+
+        Comments = new List<ProjectComment>();
+    }
+
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public int IdClient { get; private set; }
+    public int IdFreelancer {  get; private set; }
+    public decimal TotalCost { get; private set; }
+    public DateTime? StartedAt { get; private set; }
+    public DateTime? FinisheddAt { get; private set; }
+    public ProjectStatusEnum Status { get;  private set; }
+    public List<ProjectComment> Comments { get; private set; }
+}
