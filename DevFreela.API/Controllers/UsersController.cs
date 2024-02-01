99c32da7b175b51f6f0641d65f8481c2d7f5c92c
monegasco <mhoa.2000@gmail.com>
2024-02-01 11:46:03 -0300

    first commit

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+
+[Route("api/users")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get (string query)
+    {
+        //buscra ou filtras
+        return Ok();
+
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetByID(int id)
+    {
+        //buscra ou filtras
+        return Ok();
+
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateUserModel createUser)
+    {
+        //buscra ou filtras
+        return CreatedAtAction(nameof(GetByID), new { id = 1}, createUser);
+
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id,[FromBody] UpdateProjectModel updateProject)
+    {
+        if (updateProject.Description.Length > 50)
+        {
+            return BadRequest();
+        }
+        //buscra ou filtras
+        return NoContent();
+
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+       
+        //buscra ou filtras
+        return NoContent();
+
+    }
+}
---
+++ DevFreela.API/Models/CreateProjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFreela.API.Models;
+
+public class CreateProjectModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+}
