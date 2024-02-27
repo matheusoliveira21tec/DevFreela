843365a3dfe905d4b0ac69e195b6772982d135bd
monegasco <mhoa.2000@gmail.com>
2024-02-27 18:30:50 -0300

    consumindo api de pagamentos via chamada http

---
+++ DevFreela.Core/Services/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿using DevFreela.Core.DTOs;
+
+namespace DevFreela.Core.Services;
+
+public interface IPaymentService
+{
+    Task<bool> ProcessPayment(PaymentInfoDTO paymentInfoDTO);
+}
---
+++ DevFreela.Infrastructure/Payments/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿using DevFreela.Core.DTOs;
+using DevFreela.Core.Services;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Payments;
+
+public class PaymentService : IPaymentService
+{
+    public Task<bool> ProcessPayment(PaymentInfoDTO paymentInfoDTO)
+    {
+        return Task.FromResult(true);
+    }
+}
