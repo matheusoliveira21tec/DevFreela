e1c0371a86cc03a2b7dae314faab1464f0cca002
monegasco <mhoa.2000@gmail.com>
2024-02-28 18:23:06 -0300

    adicionando filtragem de dados ao getAll projects

--- DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -14,7 +14,7 @@ public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, L
 
     public async Task<List<ProjectViewModel>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
     {
-        var projects = await _projectRepository.GetAllAsync();
+        var projects = await _projectRepository.GetAllAsync(request.Query);
 
         var projectsViewModel = projects
             .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
